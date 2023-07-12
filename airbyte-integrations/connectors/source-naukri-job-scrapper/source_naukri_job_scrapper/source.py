b364645bfbcb2a59b11587aa4f8c1549d3849d2d
Surendra Puri Goswami <surendraiitkgp111@gmail.com>
2023-07-12 13:59:55 +0530

    update hirist in branch

--- airbyte-integrations/connectors/source-naukri-job-scrapper/source_naukri_job_scrapper/source.py
+++ airbyte-integrations/connectors/source-naukri-job-scrapper/source_naukri_job_scrapper/source.py
@@ -147,7 +147,91 @@ class SourceNaukriJobScrapper(Source):
         config_role = config['job_role']
         if config_role == 'dummy':
             job_roles = ["Angular Developer", "Angular JS Developer", "Associate Software Engineer","Backend Developer","C# Developer","C++ Developer","Developer","Client-Side Developer","Embedded Software Developer","Embedded Software Engineer","Front End Web Developer","Front-End Developer","Frontend Angular Developer","Frontend Architect","Frontend Developer","Frontend Engineer","Frontend Web Developer","Full Stack Developer","Full Stack Java Developer","Full Stack Software Engineer","HTML Developer","Java Backend Developer","Java Developer","Java Fullstack Developer","Java Microservices Developer","Java React Developer","Java SpringBoot Developer","Javascript Developer","Junior Software Developer","Junior Software Engineer","Mean Stack Developer","MERN Stack Developer","MIS","MIS Analyst","MIS Executive and Analyst","Node JS Developer","Node.js Developer","Python Developer","Python/Django Developer","React Developer","React Js Developer","React.js Developer","React/Frontend Developer","React+Node Js Developer","RIM Support Engineer","Ruby on Rails Developer","SAP HANA DB Administration Software Development Engineer","Software Developer","Software Development Engineer","Software Engineer","Software Engineer Trainee","Software Programmer","Solution Developer","SYBASE Database Administration Software Development Engineer","Trainee Associate Engineer","Trainee Software Developer","Trainee Software Engineer","UI Angular Developer","UI Developer","UI Frontend Developer","UI/Frontend Developer","UI/UX Developer","Web and Software Developer","Web Designer & Developer","Web Designer and Developer","Web Designer/Developer","Web Developer","Web Developer and Designer","Website Designer","website developer","XML and C# Developer","PHP Developer","Laravel Developer","Magento Developer","Drupal Developer","Dotnet developer",".net ","Vue.JS Developer","Python/Django Developer","GoLang developer","jQuery","Springboot Developer","Actuarial Analyst","Analyst","AR Analyst","Associate Business Analyst","Automation Test Analyst","Azure Data Engineer","Big Data Engineer","Business Analyst","Business Data Analyst","Data Analyst","Data Analytics Trainer","Data Research Analyst","Data Researcher","Data Science Engineer","Data Scientist","Database Administrator","Functional Analyst","Junior Analyst","Junior Research Analyst","KYC Analyst","Market Research Analyst","Power BI Developer","Product Analyst","Programmer Analyst","QA Analyst","Quality Analyst","Real Time Analyst","Reconciliation Analyst","Research Analyst","Risk Analyst","Sales Analyst","Salesforce Business Analyst","Service Desk Analyst","SOC Analyst","SQL Developer","Android Application Developer","Android Developer","Android Mobile Application Developer","Application Developer","Application Support Engineer","Flutter Developer","iOS Application Developer","IOS Developer","Mobile App Developer","Mobile Application Developer","Associate Technical Support Engineer","Automation Engineer","Automation Test Engineer","Batch Support Engineer","Desktop Support Engineer","Genesys Support Engineer","IT Support Engineer","Network Support Engineer","QA Automation Engineer","SaaS Support Engineer","Security Engineer","Test Automation Engineer","Systems Support Engineer","Software Development Engineer - Test","Software Test Engineer","Software Tester","Support Engineer","Tech Customer Support Engineer","Technical Support Engineer","Servicenow Developer","SharePoint Developer","Shopify Developer","Unity Game Developer","WordPress & Shopify Developer","WordPress Developer","Wordpress Web Developer","Unreal Developer"]
+            for query in job_roles:
+                has_more = True
+                page_no = 0
+                while has_more:
+                    url = f"https://jobseeker-api.hirist.com/v2/jobfeed/-1/v3/search?pageNo={page_no}&query={query}&loc=&minexp=0&maxexp=2&range=0&boost=1"
+                    try:
+                        resp = requests.get(url).json()
+                    except Exception as e:
+                        print(str(e))
+                        break
+                    jobs = resp.get('jobs') or []
+                    for job in jobs:
+                        skills = {"preferredSkills": [], "otherSkills": []}
+                        tags = job.get('tags') or []
+                        for tag in tags:
+                            skill_name = tag.get('name')
+                            is_mandatory = tag.get('isMandatory')
+                            if is_mandatory:
+                                skills['preferredSkills'].append(skill_name)
+                            else:
+                                skills['otherSkills'].append(skill_name)
 
+                        locations = job.get('locations')
+
+                        location_name = locations[0].get('name') if locations else ""
+
+                        company_data = job.get('companyData')
+                        if company_data:
+                            company_name = company_data.get('companyName')
+                            company = {
+                                "name": company_name
+                            }
+
+                            yield AirbyteMessage(
+                                    type=Type.RECORD,
+                                    record=AirbyteRecordMessage(
+                                            stream='companies', data=company, emitted_at=int(datetime.now().timestamp()) * 1000
+                                    )
+                            )
+                        else:
+                            company_name = ""
+
+                        job_opening = {
+                            "job_role": query,
+                            "job_title": job.get('jobdesignation') or "",
+                            "min_experience": job.get('min') or "",
+                            "max_experience": job.get('max') or "",
+                            "job_description_url": job.get('jobDetailUrl') or "",
+                            "skills": skills,
+                            "min_ctc": job.get('minSal') or "",
+                            "max_ctc": job.get('maxSal') or "",
+                            "company": company_name,
+                            "job_source": "hirist",
+                            "job_location": location_name,
+                            "raw_response": job,
+                            "job_description_url_without_job_id": job.get('jobDetailUrl') or "",
+                            "job_description_raw_text": job.get('introText') or ""
+                        }
+
+                        yield AirbyteMessage(
+                                type=Type.RECORD,
+                                record=AirbyteRecordMessage(
+                                        stream='job_openings', data=job_opening, emitted_at=int(datetime.now().timestamp()) * 1000
+                                ),
+                        )
+
+                        recruiter = job.get('recruiter')
+                        if recruiter:
+                            recruiter_details = {
+                                "hiring_manager_for_job_link": job.get('jobDetailUrl'),
+                                "name": recruiter.get('recruiterName'),
+                                "company": company_name,
+                                "short_intro": recruiter.get('designation'),
+                                "linkedin_profile_url": f"dummy_{company_name}"
+                            }
+                            yield AirbyteMessage(
+                                    type=Type.RECORD,
+                                    record=AirbyteRecordMessage(
+                                            stream='recruiter_details', data=recruiter_details,
+                                            emitted_at=int(datetime.now().timestamp()) * 1000
+                                    ),
+                            )
+
+                    has_more = resp.get('hasMore')
+                    page_no += 1
         else:
             job_roles = [config_role]
 
