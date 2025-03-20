4edfa13fb5dd4e2e47932acf923f8089dda219d9
Gaurav Kapatia <gkapatia@gmail.com>
2025-03-20 19:34:04 +0530

    feat: add Source field to stream fields in SourceLeadsquareLeads

--- airbyte-integrations/connectors/source-leadsquare-leads/source_leadsquare_leads/source.py
+++ airbyte-integrations/connectors/source-leadsquare-leads/source_leadsquare_leads/source.py
@@ -92,7 +92,8 @@ class SourceLeadsquareLeads(Source):
         "mx_Reactivation_Source",
         "mx_Lead_Status",
         "mx_PMM_Identifier",
-        "OwnerId"
+        "OwnerId",
+        "Source"
     ]
 
     def get_stream_fields(self):
